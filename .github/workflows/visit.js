9f30bb4f7150f935a7c5e8f15e2a4d85210554df
zanidar <50781310+zanidar@users.noreply.github.com>
2025-06-23 14:06:57 -0300

    Update visit.js

--- .github/workflows/visit.js
+++ .github/workflows/visit.js
@@ -1,14 +1,2 @@
-const puppeteer = require('puppeteer');
 
-(async () => {
-  const browser = await puppeteer.launch({
-    headless: true,
-    args: ['--no-sandbox', '--disable-setuid-sandbox']
-  });
-
-  const page = await browser.newPage();
-  const url = 'https://telegram-bot-cron.onrender.com'; // ⬅️ Cambia esto por tu URL real
-  await page.goto(url, { waitUntil: 'networkidle2' });
-  console.log(`✅ Sitio visitado: ${url}`);
-  await browser.close();
-})();
+a
